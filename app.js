2a5e4432138b1ea7dc13bdb397444b07c06cb98c
Sunil Sadasivan <sunil@buffer.com>
2017-02-06 14:14:33 -0800

    Initial checkin of /apply POST boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+var env = process.env.NODE_ENV || 'development';
+
+//load dev vars from local config
+if(env == 'development') {
+  require('dotenv').config();
+}
+
 var express = require('express');
+var controller = require('./lib/controller');
 var app = express();
 
 //Enable public static file access
 app.use(express.static('public'))
+app.post('/apply', controller.apply);
 
 app.listen(3234);
 console.log('Running on port 3234')
