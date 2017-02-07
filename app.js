0e737e0be2c92375173c3807f14e3a5d1e9181c6
Sunil Sadasivan <sunil@buffer.com>
2017-02-06 20:11:12 -0800

    Setup csrf protection

--- app.js
+++ app.js
@@ -5,13 +5,29 @@ if(env == 'development') {
   require('dotenv').config();
 }
 
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
+
 var express = require('express');
 var controller = require('./lib/controller');
+
+// setup route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+var { errorHandler } = require('./lib/errors')
+
 var app = express();
+
+// middleware init
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(csrf({ cookie: true }))
+app.use(errorHandler)
 
 //Enable public static file access
 app.use(express.static('public'))
-app.post('/apply', controller.apply);
+app.post('/apply', parseForm, csrfProtection, controller.apply);
 
 app.listen(3234);
 console.log('Running on port 3234')
