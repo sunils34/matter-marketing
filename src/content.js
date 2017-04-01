4258f26ba342d2308d215c85bb9ba189b504dcb1
eli <eli.fitch.prof@gmail.com>
2017-03-31 23:17:00 -0400

    highlight

--- src/content.js
+++ src/content.js
@@ -5,5 +5,8 @@ module.exports = {
   hero: {
     headline: 'Helping tech companies build a more inclusive and diverse culture.',
     button: 'Get your own diversity dashboard'
+  },
+  highlight: {
+    headline: 'There’s a gap in your diversity efforts.'
   }
 }
