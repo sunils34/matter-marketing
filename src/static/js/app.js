c13422a65eb6bbddf4d39019bd582b1518da55e6
eli <eli.fitch.prof@gmail.com>
2017-04-05 21:41:40 -0400

    tour

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,9 +1,12 @@
 import slider from './components/slider';
+import tour from './components/tour';
 import processAnimation from './components/process-animation';
 
 slider('.js-feature-slider');
-slider('.js-card-slider', {
-  prevNextButtons: false,
-  pageDots: false
-});
-processAnimation('.js-anim-process');
+// slider('.js-card-slider', {
+//   prevNextButtons: false,
+//   pageDots: false
+// });
+tour('.js-tour');
+// processAnimation('.js-anim-process');
+
--- src/static/js/components/tour.js
+++ src/static/js/components/tour.js
@@ -1,8 +1,24 @@
 // inspired by http://codepen.io/sdras/pen/VjvGJM
-import TimelineMax from 'gsap';
+import TweenMax from 'gsap';
 
-export function tour(selector) {
-  const tl = new TimelineMax;
+export default function tour(selector) {
+  const tl = new TimelineMax();
+  tl.pause();
+  const el = document.querySelector(selector);
+  const sbj = el.querySelector('.js-tour-subject');
+  const steps = el.querySelectorAll('.js-tour-step');
+  const amt= 75;
+
+  steps.forEach((step, index) => {
+    const target = step.getBBox();
+    // const newView = "" + (s.x - amt) + " " + (s.y - amt) + " " + (s.width + amt*2) + " " + (s.height + amt*2);
+    const newView = `${(target.x - amt)} ${(target.y - amt)} ${(target.width + amt * 2)} ${(target.height + amt * 2)}`;
+    tl.to(sbj, 2, {
+      attr: { viewBox: newView },
+      ease: Power4.easeInOut
+    })
+  });
+  tl.play();
 
 // function moveChart(origin, end) {
 //   $(origin).on("click", function() {
@@ -16,4 +32,15 @@ export function tour(selector) {
 //       });
 //   });
 // }
+}
+
+function moveTo(pos) {
+  var moveTo = document.getElementById(end);
+  var s = moveTo.getBBox();
+  var amt = 75;
+  newView = "" + (s.x - amt) + " " + (s.y - amt) + " " + (s.width + amt*2) + " " + (s.height + amt*2);
+  TweenMax.to(graphic, 1.5, {
+    attr: { viewBox: newView },
+    ease:Circ.easeOut
+  });
 }
