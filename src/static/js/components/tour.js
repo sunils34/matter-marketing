5abf1bf7f7337dd8626e1d4664db38c9d3daa880
eli <eli.fitch.prof@gmail.com>
2017-04-05 21:18:04 -0400

    tour structure

---
+++ src/static/js/components/tour.js
@@ -0,0 +1,19 @@
+// inspired by http://codepen.io/sdras/pen/VjvGJM
+import TimelineMax from 'gsap';
+
+export function tour(selector) {
+  const tl = new TimelineMax;
+
+// function moveChart(origin, end) {
+//   $(origin).on("click", function() {
+//     var moveTo = document.getElementById(end);
+//     var s = moveTo.getBBox();
+//     var amt = 75;
+//     newView = "" + (s.x - amt) + " " + (s.y - amt) + " " + (s.width + amt*2) + " " + (s.height + amt*2);
+//     TweenMax.to(graphic, 1.5, {
+//         attr: { viewBox: newView },
+//         ease:Circ.easeOut
+//       });
+//   });
+// }
+}
