9625df6c078bc06d05d407c3194e230652e06511
eli <eli.fitch.prof@gmail.com>
2017-04-06 22:38:17 -0400

    simplify anim

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,9 +1,10 @@
 import slider from './components/slider';
 import tour from './components/tour';
-import { pinScroll } from './components/pin-scroll';
+import { pinScroll, pinSlideIn } from './components/pin-scroll';
 import { simplifyAnimation } from './components/simplify-animation';
 
 slider('.js-feature-slider');
 tour('.js-tour');
 pinScroll('.js-pin-scroll');
 // pinScroll('.js-pin-from-sides');
+pinSlideIn('.js-pin-slide-in')
--- src/static/js/components/pin-scroll.js
+++ src/static/js/components/pin-scroll.js
@@ -1,4 +1,6 @@
 import ScrollMagic from 'scrollmagic';
+import asdf from '../vendor/scrollmagic-gsap-plugin';
+import TweenMax from 'gsap';
 
 const scrollCtrl = new ScrollMagic.Controller({
   globalSceneOptions: {
@@ -18,6 +20,34 @@ export function pinScroll(selector, opts = {
   .addTo(scrollCtrl);
 }
 
-export function pinWithAnimation() {
+export function pinSlideIn(selector) {
+  const triggerElement = document.querySelector(selector);
+  const cards = triggerElement.querySelectorAll('.js-card-slide-in');
+  const line1 = cards[0].querySelector('.js-animate-line');
+  const line2 = cards[1].querySelector('.js-animate-line');
 
+  TweenMax.set(cards[0], {x: '-100%', opacity: 0});
+  TweenMax.set(cards[1], {x: '100%', opacity: 0});
+  TweenMax.set(line1, {scaleX: '0%'});
+  TweenMax.set(line2, {scaleX: '0%'});
+
+  const tl = new TimelineMax();
+  tl.to(cards[0], 1, {
+    x: '0%', opacity: 1
+  })
+  .to(cards[1], 1, {
+    x: '0%', opacity: 1
+  }, '-=1')
+  .to(line1, 1, {
+    scaleX: 1
+  })
+  .to(line2, 1, {
+    scaleX: 1
+  }, '-=1');
+
+  new ScrollMagic.Scene({
+    triggerElement
+  })
+  .setTween(tl)
+  .addTo(scrollCtrl);
 }
