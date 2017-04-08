9bfcea7ff7e415046150b5e4e6d3bd74bd2d69f7
eli <eli.fitch.prof@gmail.com>
2017-04-08 16:15:27 -0400

    mo edge case

--- src/static/js/components/pin-scroll.js
+++ src/static/js/components/pin-scroll.js
@@ -3,8 +3,8 @@ import scrollGsap from '../vendor/scrollmagic-gsap-plugin';
 import TweenMax from 'gsap';
 import debounce from 'lodash.debounce';
 
-
 const scrollCtrl = new ScrollMagic.Controller();
+const SMALL_BREAKPOINT = 640;
 
 export function pinScroll(selector, opts = {
   release: false,
@@ -27,24 +27,46 @@ export function pinSlideIn(selector) {
   const line1 = cards[0].querySelector('.js-animate-line');
   const line2 = cards[1].querySelector('.js-animate-line');
 
-  TweenMax.set(cards[0], {x: '-100%', opacity: 0});
-  TweenMax.set(cards[1], {x: '100%', opacity: 0});
-  TweenMax.set(line1, {scaleX: '0%'});
-  TweenMax.set(line2, {scaleX: '0%'});
-
   const tl = new TimelineMax();
-  tl.to(cards[0], 1, {
-    x: '0%', opacity: 1
-  })
-  .to(cards[1], 1, {
-    x: '0%', opacity: 1
-  }, '-=1')
-  .to(line1, 1, {
-    scaleX: 1
-  })
-  .to(line2, 1, {
-    scaleX: 1
-  }, '-=1');
+
+  if (window.innerWidth < SMALL_BREAKPOINT) {
+    TweenMax.set(cards[0], {y: '-100%', opacity: 0});
+    TweenMax.set(cards[1], {y: '100%', opacity: 0});
+    TweenMax.set(line1, {scaleY: '0%'});
+    TweenMax.set(line2, {scaleY: '0%'});
+
+    tl.to(cards[0], 1, {
+      y: '0%', opacity: 1
+    })
+    .to(cards[1], 1, {
+      y: '0%', opacity: 1
+    }, '-=1')
+    .to(line1, 1, {
+      scaleY: 1
+    })
+    .to(line2, 1, {
+      scaleY: 1
+    }, '-=1');
+
+  } else {
+    TweenMax.set(cards[0], {x: '-100%', opacity: 0});
+    TweenMax.set(cards[1], {x: '100%', opacity: 0});
+    TweenMax.set(line1, {scaleX: '0%'});
+    TweenMax.set(line2, {scaleX: '0%'});
+
+    tl.to(cards[0], 1, {
+      x: '0%', opacity: 1
+    })
+    .to(cards[1], 1, {
+      x: '0%', opacity: 1
+    }, '-=1')
+    .to(line1, 1, {
+      scaleX: 1
+    })
+    .to(line2, 1, {
+      scaleX: 1
+    }, '-=1');
+  }
 
   new ScrollMagic.Scene({
     triggerElement,
