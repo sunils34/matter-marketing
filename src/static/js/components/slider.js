4108532cb25e77182e990a7089cb5abc36f7ffdd
eli <eli.fitch.prof@gmail.com>
2017-04-09 21:15:33 -0400

    more clean

--- src/static/js/components/slider.js
+++ src/static/js/components/slider.js
@@ -1,6 +1,8 @@
 import Flickity from 'flickity';
 
-export default function slider(selector, options) {
+export default function slider(selector, options = {
+  wrapAround: true
+}) {
   var elem = document.querySelector(selector);
   return new Flickity( elem, options);
 }
